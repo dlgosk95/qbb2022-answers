18d34ff4d0600937a98e23360a50aaa21195b71d
Haena <hlee273@jh.edu>
2022-10-09 17:11:04 -0400

    refined graphs

--- week4-homework/gwas_data/assignment5.py
+++ week4-homework/gwas_data/assignment5.py
@@ -2,22 +2,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 8 is p value
-# 0 is chr
-# 2 is BP
-# scatter plot
-# want nested numpy array [[1,231, .23423], ...]
-# first row is header
+## 8 is p value
+## 0 is chr
+## scatter plot
+## want nested numpy array [[1,231, .23423], ...]
+## first row is header
 
 association = np.genfromtxt("output_CB1908.assoc.linear", dtype = None, encoding = None, names = ["chr", "snp", "bp", "a1", "test", "nmiss", "beta", "stat", "pvalue"])
 # print(association[0:4])
 
 dictionary = {}
 sig_list = []
-number = 0 # to index
+number = 0 ## to index
 # print (association[chr, 1])
 
-# make for loop
+## make for loop
 for i in association[1:]:
     if int(i["chr"]) not in dictionary.keys():
         dictionary[int(i["chr"])] = []
@@ -27,8 +26,8 @@ for i in association[1:]:
     number += 1
 
 
-print(dictionary[1][:4])
-# [[0, 1.1563935280754891], [1, 0.486516043295743], [2, 0.661145253747677], [3, 0.027612000892527164]]
+# print(dictionary[1][:4])
+## [[0, 1.1563935280754891], [1, 0.486516043295743], [2, 0.661145253747677], [3, 0.027612000892527164]]
 ## assuming they are ordered, and they are.
 
 sig = np.array(sig_list).T
@@ -40,35 +39,19 @@ x_labels_pos = []
 for key, value in dictionary.items():
     # print(value[:4])
     value = np.array(value).T
-    # Transpose: switch one list per row like a matrix
-    # x, y = value # ValueError: too many values to unpack (expected 2)
+    ## Transpose: switch one list per row like a matrix
+    ## x, y = value # ValueError: too many values to unpack (expected 2)
     ax.scatter(x = value[0], y = value[1], color=colors[key % len(colors)], s = 2)
     x_labels.append(str(key))
-    # x_labels_pos.append((group['ind'].iloc[-1] - (group['ind'].iloc[-1] - group['ind'].iloc[0])/2))
     x_labels_pos.append((max(value[0])-min(value[0]))/2 + min(value[0]))
 ax.scatter(sig[0], sig[1], color = "red", s = 2)
 ax.set_xticks(x_labels_pos)
-ax.set_xticklabels(x_labels)
+ax.set_xticklabels(x_labels, fontsize = 8)
 ax.axhline(y=5)
 ax.set_xlabel("Chromosomes")
 ax.set_ylabel("-log10(P-value)")
-
-## have lables at correct spots
-## axes labels
 plt.savefig("manhattan_plot.png")
 plt.show()
 plt.close()
-
-
-
-
-
-
-
-
-
-
-
-
 
 
